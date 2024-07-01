b13fe9f8ee4ff5903ec2924cd6bbf7a57967a964
Darshan Chhetri <mitch4827@gmail.com>
2024-07-01 13:54:40 +0545

    camp button

--- src/sections/Camp.jsx
+++ src/sections/Camp.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import camp3 from '../assets/Camp Photos/10.jpg';
+import React from "react";
+import camp3 from "../assets/Camp Photos/10.jpg";
+import { Link } from "react-router-dom";
 
 const Camp = () => {
   return (
@@ -7,15 +8,22 @@ const Camp = () => {
       <div className="p-3 flex-col md:flex-row h-full flex justify-center items-center md:mx-10 my-11">
         <div className="md:w-[40%] mt-10">
           <h1 className="text-5xl md:text-6xl font-bold">
-           Health <span className="text-blue-400">Camp</span> 
+            Health <span className="text-blue-400">Camp</span>
           </h1>
-          <p className="text-xl py-6">
-           Health camp conducted successfully.
-          </p>
+          <p className="text-xl py-6">Health camp conducted successfully.</p>
+          <Link to="/gallery">
+            <button className="mt-5 btn btn-outline btn-info">
+              Watch All Photos
+            </button>
+          </Link>
         </div>
 
         <div className="md:w-[60%] md:h-full md:py-10">
-          <img src={camp3} className="w-full h-full object-cover" alt="Health Camp" />
+          <img
+            src={camp3}
+            className="w-full h-full object-cover"
+            alt="Health Camp"
+          />
         </div>
       </div>
     </div>
