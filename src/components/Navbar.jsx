0738be3c4d0bf2cbafa717b254dc78d7b7eafa73
Darshan Chhetri <mitch4827@gmail.com>
2024-07-06 22:38:07 +0545

    nav number updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -92,7 +92,7 @@ const Navbar = () => {
           {/*button */}
           <div className="flex justify-center items-center">
             <Link
-              to="tel:+9779845176825"
+              to="tel:015201929"
               className="inline-block font-bold py-2 px-4 rounded-xl transition duration-300"
             >
               <img src={call} className="w-5 h-5" alt="" />
